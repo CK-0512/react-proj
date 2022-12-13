f9f973abca6ff2b7f054187c721930c3040a1db7
basdzxqwe <ock6166@naver.com>
2022-12-13 20:17:00 +0900

    setTimeout 예제

--- Desktop/work/use-state-ex/src/StopWatch.js
+++ Desktop/work/use-state-ex/src/StopWatch.js
@@ -1,9 +1,12 @@
-import React from "react";
+import Reactm, {useState} from "react";
 
 function StopWatch() {
+  const [num, setNum] = useState(0);
+
+  setTimeout(() => setNum(num + 1), 1000);
   return (
   <>
-    <div>안녕</div>
+    숫자 : {num}
   </>
   );
 }
