99056bccc8fa8bf2ec41e03cd99060a91b27d953
basdzxqwe <ock6166@naver.com>
2022-12-13 20:27:59 +0900

    일시정지

--- Desktop/work/use-state-ex/src/StopWatch.js
+++ Desktop/work/use-state-ex/src/StopWatch.js
@@ -3,10 +3,15 @@ import Reactm, {useState} from "react";
 function StopWatch() {
   const [num, setNum] = useState(0);
 
-  setTimeout(() => setNum(num + 1), 1000);
+  const timeoutId = setTimeout(() => setNum(num + 1), 1000);
+
+  const pause = () => clearTimeout(timeoutId);
   return (
   <>
     숫자 : {num}
+
+    <hr />
+    <button onClick={pause}>일시정지</button>
   </>
   );
 }
