406d9db1d7bf76f3d4cdfafabe1eb744ae301a8e
basdzxqwe <ock6166@naver.com>
2022-12-13 19:30:12 +0900

    use-state-ex 시작

---
+++ Desktop/work/use-state-ex/src/App.js
@@ -0,0 +1,9 @@
+import StopWatch from "./StopWatch"
+
+function App() {
+  return (
+    < StopWatch />
+  );
+}
+
+export default App;
---
+++ Desktop/work/use-state-ex/src/StopWatch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StopWatch() {
+  return (
+  <>
+    <div>안녕</div>
+  </>
+  );
+}
+
+export default StopWatch;
