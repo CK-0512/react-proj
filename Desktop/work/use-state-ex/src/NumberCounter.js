abff7dcf34ac68bee9db1021e6b43de8d322450a
basdzxqwe <ock6166@naver.com>
2022-12-15 18:30:55 +0900

    or 연산자

--- Desktop/work/use-state-ex/src/NumberCounter.js
+++ Desktop/work/use-state-ex/src/NumberCounter.js
@@ -4,19 +4,15 @@ function NumberCounter() {
   const [no, setNo] = useState(0);
 
   const noIsEvenDiv = 
-  no % 2 == 0 ?(
   <>
-  <hr />
-  <span>짝수입니다.</span>
+  <hr />{no % 2 == 0 ?<div>짝수입니다.</div> : <div>홀수입니다.</div>}
   </>
-  ) : (
-  <></>
-  );
+  ;
 
   const noIs8MultipleDiv =
-  no % 8 == 0 && (<>
+  no % 8 == 0 || (<>
     <hr />
-    <span>8의 배수입니다..</span>
+    <div>8의 배수가 아닙니다.</div>
   </>);
 
   return (
