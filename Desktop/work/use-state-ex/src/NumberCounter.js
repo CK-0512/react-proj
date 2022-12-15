80fa7b61d8845237dab6eebfc0f4169463194863
basdzxqwe <ock6166@naver.com>
2022-12-15 18:22:36 +0900

    if를 사용해서 짝수일 때만 짝수라는 문구가 나오도록

--- Desktop/work/use-state-ex/src/NumberCounter.js
+++ Desktop/work/use-state-ex/src/NumberCounter.js
@@ -3,11 +3,21 @@ import React, { useState } from "react";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
+  const noIsEvenDiv = 
+  no % 2 == 0 ?(
+  <>
+  <hr />
+  <span>짝수입니다.</span>
+  </>
+  ) : (
+  <></>
+  );
   return (
     <>
     숫자 : { no }
     <hr />
     <button onclick={() => setNo(no + 1)}>증가</button>
+    {noIsEvenDiv}
     </>
   );
 }
