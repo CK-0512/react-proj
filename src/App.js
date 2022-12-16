fa70aac934776a0e615143a1472e8d0e69472165
basdzxqwe <ock6166@naver.com>
2022-12-16 18:24:16 +0900

    폰트를 일일히 변경

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           }}
           >
           <img src="https://picsum.photos/id/201/400/400" />
-          <div style={{textAlign:'center'}}>MAC BOOK AIR</div>
+          <div style={{textAlign:'center', fontWeight:'bold', color:'#454545'}}>MAC BOOK AIR</div>
           <div style={{textAlign:'center'}}>4,340,000</div>
         </div>
 
@@ -29,7 +29,7 @@ function App() {
           }}
           >
           <img src="https://picsum.photos/id/1/400/400" />
-          <div style={{textAlign:'center'}}>MAC BOOK PRO</div>
+          <div style={{textAlign:'center', fontWeight:'bold', color:'#454545'}}>MAC BOOK PRO</div>
           <div style={{textAlign:'center'}}>4,340,000</div>
         </div>
 
@@ -41,7 +41,7 @@ function App() {
           }}
           >
           <img src="https://picsum.photos/id/2/400/400" />
-          <div style={{textAlign:'center'}}>MAC BOOK PRO PLUS</div>
+          <div style={{textAlign:'center', fontWeight:'bold', color:'#454545'}}>MAC BOOK PRO PLUS</div>
           <div style={{textAlign:'center'}}>4,340,000</div>
         </div>
       </div>
