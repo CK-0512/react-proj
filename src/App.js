9628588699155d3e231d965812d2e4c5c9fdfe4d
basdzxqwe <ock6166@naver.com>
2022-12-16 17:57:10 +0900

    팝업과 닫기버튼

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import StopWatch from "./StopWatch"
 import NumberCounter from "./NumberCounter";
+import Popup from "./Popup";
 
 function App() {
   return (
     <>
     < StopWatch />
     < NumberCounter />
-    
+    < Popup />
     </>
   );
 }
