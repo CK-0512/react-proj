415fc426674f62991b21d0062cca51ac3add084c
basdzxqwe <ock6166@naver.com>
2022-12-19 21:39:44 +0900

    숫자가 쌓이도록

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
 
 function NoRecord() {
-    const [arr, setArr] = useState([1, 2, 3, 4, 5]);
-
-    setTimeout(() => {
-        setArr([...arr, arr.length + 1])
-    }, 1000);
-
     const [no, setNo] = useState(0);
-    const [recordNo, setRecordNo] = useState(0);
+    const [recordNos, setRecordNos] = useState([]);
+
     const saveNo = () => {
-        setRecordNo(no);
+        setRecordNos([...recordNos, no]);
     }
     return (
         <>
-        {arr.join(",")}
-            {/* <h1>숫자기록</h1>
+            <h1>숫자기록</h1>
             <input type="number" 
             value={no} 
             onChange={(e) => setNo(e.target.valueAsNumber)} 
@@ -24,7 +18,7 @@ function NoRecord() {
                 기록
             </button>
             <hr />
-            기록된 숫자 : {recordNo} */}
+            기록된 숫자 : [{recordNos.join(",")}]
         </>
     )
 }
